5e498688f80bc1a126e3ee320fdfc23f0b6fdaa3
TruStorey <tech@neonmumbles.com>
2025-06-06 00:49:47 +0100

    Added shadcn, cleared layout and added themprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import './globals.css'
-import { ReactNode } from 'react'
-import Sidebar from './components/Sidebar'
-
+import "./globals.css";
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
 export const metadata = {
-  title: 'Echo Jot',
-  description: 'Local-first markdown journaling app',
-}
+  title: "Echo Jot",
+  description: "Local-first markdown journaling app",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className="flex h-screen bg-gray-50 text-gray-900">
-        <Sidebar />
-        <main className="flex-1 overflow-auto p-6">
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
+  );
 }
